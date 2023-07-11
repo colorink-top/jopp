5ea0f888fd9dfb8ec9712d4e9d8de1b24b9510ac
suziwen <suziwen1@gmail.com>
2023-07-11 14:08:39 +0800

    不需要 base path

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 
   kit: {
     paths: {
-      base: dev ? '' : process.env.BASE_PATH
+//      base: dev ? '' : process.env.BASE_PATH
     },
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
